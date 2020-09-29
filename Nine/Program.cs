79d0888f1dab6dda1365376d29406c1fe67c248c
VisualAcademy <redplus@redplus.net>
2020-09-29 21:03:17 +0000

    init setter

--- Nine/Program.cs
+++ Nine/Program.cs
@@ -1,3 +1,17 @@
-﻿using static System.Console;
+﻿var sponsor = new Sponsor
+{
+    Id = 1,
+    DisplayName = "Red"
+};
+//sponsor.DisplayName = "RedPlus"; // mutable => init => immutable
 
-WriteLine("Hello World!");
+DisplayData(sponsor); 
+
+static void DisplayData(Sponsor sponsor) => 
+    System.Console.WriteLine($"{sponsor.Id} - {sponsor.DisplayName}");
+
+struct Sponsor
+{
+    public int Id { get; init; }
+    public string DisplayName { get; init; }
+}
