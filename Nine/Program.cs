b494e7bc313bba9e1a2615d7643618c2a13a2283
VisualAcademy <redplus@redplus.net>
2020-10-02 00:55:52 +0000

    Args and Await

--- Nine/Program.cs
+++ Nine/Program.cs
@@ -1,9 +1,9 @@
-﻿var sponsor = new Sponsor
-{
-    Id = 1,
-    DisplayName = "Red"
-};
-//sponsor.DisplayName = "RedPlus"; // mutable => init => immutable
+﻿//[1] Top Level Statements
+using System.Threading.Tasks;
+System.Console.WriteLine($"Hello {args[0]}, Happy Coding!!!");
+await Task.Delay(3000);
+
+var sponsor = new Sponsor { Id = 1, DisplayName = "Red" }; //sponsor.DisplayName = "RedPlus"; // mutable => init => immutable
 
 DisplayData(sponsor); 
 
